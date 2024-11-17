a4c4e16e2d77b1524475e01a3fab6b642a7e13bb
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-11-17 15:02:02 +0900

    commit

---
+++ test_connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "myspl";
+
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
